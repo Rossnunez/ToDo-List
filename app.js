3374274d9e79545190b22e0790bf0fdcfd590b9e
Rosseleen Nunez <nrosseleen@gmail.com>
2021-03-23 22:08:00 -0400

    installed ejs and added some sample code

--- app.js
+++ app.js
@@ -5,7 +5,14 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.get("/", function(req, res){
-  res.send("Hello");
+  var today = new Date();
+  var currentDay = today.getDay();
+  if(currentDay === 6 || currentDay === 0){
+      res.write("<h1>Yay it's the weekend</h1>");
+  } else {
+    res.sendFile(__dirname + "/index.html");
+  }
+
 });
 
 app.listen(3000, function(){
