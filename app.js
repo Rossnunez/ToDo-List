40cd85509749ee224b06cc9884b3e7ccca44a0a4
Rosseleen Nunez <nrosseleen@gmail.com>
2021-03-24 00:48:48 -0400

    created a form where the user can populate a list through the go button

--- app.js
+++ app.js
@@ -3,33 +3,28 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
+var items = ["Buy Food", "Cook Food", "Eat em"];
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
-
 app.get("/", function(req, res){
   var today = new Date();
   var currentDay = today.getDay();
-  var day = "";
+ var options = {
+   weekday: "long",
+   day: "numeric",
+   month: "long"
+ };
 
-  if(currentDay === 6){
-    day = "Saturday";
-  } else if(currentDay === 0){
-    day = "Sunday";
-  } else if(currentDay === 1){
-    day = "Monday";
-  } else if(currentDay === 2){
-    day = "Tuesday";
-  } else if(currentDay === 3){
-    day = "Wednesday";
-  } else if(currentDay === 4){
-    day = "Thursday";
-  } else if(currentDay === 5){
-    day = "Friday";
-  } else {
-    console.log("Error:current day is equal to " + currentDay);
-  }
+ var day = today.toLocaleDateString("en-US", options);
+  res.render("list", {kindOfDay: day, newListItems: items});
+});
 
-  res.render("list", {kindOfDay: day});
+app.post("/", function(req, res){
+  var item = req.body.newItem;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, function(){
