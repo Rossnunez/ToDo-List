8135a80963373ee71c9a3ec3861dfde18e53965f
Rosseleen Nunez <nrosseleen@gmail.com>
2021-03-24 22:28:27 -0400

    changed var to let, implemented css template to app.js and list.ejs

--- app.js
+++ app.js
@@ -3,26 +3,30 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const app = express();
-var items = ["Buy Food", "Cook Food", "Eat em"];
+
+let items = ["Buy Food", "Cook Food", "Eat em"];
 
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
 
 app.set('view engine', 'ejs');
+
 app.get("/", function(req, res){
-  var today = new Date();
-  var currentDay = today.getDay();
- var options = {
+
+  let today = new Date();
+
+  let options = {
    weekday: "long",
    day: "numeric",
    month: "long"
  };
 
- var day = today.toLocaleDateString("en-US", options);
+ let day = today.toLocaleDateString("en-US", options);
   res.render("list", {kindOfDay: day, newListItems: items});
 });
 
 app.post("/", function(req, res){
-  var item = req.body.newItem;
+  let item = req.body.newItem;
   items.push(item);
   res.redirect("/");
 });
