ed5a190b4bf90d38c914d234d04371a32397a95a
Rosseleen Nunez <nrosseleen@gmail.com>
2021-03-26 23:20:21 -0400

    connected task data with mongoose

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 //jshint esversion:6
 const express = require("express");
 const bodyParser = require("body-parser");
-
-const { JSDOM } = require( "jsdom" );
-const { window } = new JSDOM( "" );
+const mongoose = require("mongoose");
 
 const app = express();
 
@@ -12,20 +10,47 @@ let items = ["Buy Food", "Cook Food", "Eat em"];
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.connect("mongodb://localhost:27017/todolistDB", {useNewUrlParser: true,  useUnifiedTopology: true });
+
 app.set('view engine', 'ejs');
+
+const taskSchema = new mongoose.Schema({
+  task: {
+    type: String,
+    required: [true, "Task Name is required"]
+  }
+});
+
+const Task = mongoose.model("Task", taskSchema);
+
+const task1 = new Task({
+  task :  "Do the dishes"
+});
+
+const task2 = new Task ({
+  task : "Wash the car"
+});
+
+const task3 = new Task ({
+  task : "Buy some milk"
+});
+
+const defaultTasks = [task1, task2, task3];
+
+Task.insertMany(defaultTasks, function(err){
+  if(err){
+    console.log(err);
+  } else {
+    console.log("Default task were added successfully");
+  }
+});
 
 app.get("/", function(req, res){
 
-  let today = new Date();
-  let options = {
-   weekday: "long",
-   day: "numeric",
-   month: "long"
- };
+  res.render("list", {kindOfDay: "Today", newListItems: items});
 
-  let day = today.toLocaleDateString("en-US", options);
-  res.render("list", {kindOfDay: day, newListItems: items});
 });
+
 
 app.post("/", function(req, res){
   let item = req.body.newItem;
