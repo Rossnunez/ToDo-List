dfafc31f93390dde9047d8880421504df455496c
Rosseleen Nunez <nrosseleen@gmail.com>
2021-03-27 22:33:02 -0400

    custome list is always set to lowercase except for the first letter

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
+const _ = require("lodash");
 
 const app = express();
 
@@ -63,7 +64,7 @@ app.get("/", function(req, res){
 
 //
 app.get("/:customListName", function(req, res){
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({name: customListName}, function(err, foundList){
     if(!err){
@@ -111,7 +112,7 @@ app.post("/", function(req, res){
 app.post("/delete", function(req, res){
   const checkedTaskId = req.body.checkbox;
   const listName = req.body.listName;
-  console.log(listName);
+
   if(listName === "Today"){
     Task.findByIdAndRemove(checkedTaskId, function(err){
       if(err){
